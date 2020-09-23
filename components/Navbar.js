a033fb89d1228c3e4a2513e8a66ce8e4f0f5cd5a
Kern Elder <kelel1@yahoo.com>
2020-09-23 02:04:38 -0400

    add media queries for navbar links

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,74 +3,86 @@ import React, { useState } from 'react';
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
-
   const handleClick = () => setClick(!click);
 
+  
   return (
-  <header className="navbar">
+    <header className="navbar">
+      <nav className="nav_navigation">
+        <div></div>
+        <div className="nav_logo"><a href="/">Daiper-Cakez</a></div>
+        <div className="space"></div>
+        <div className="nav_items">
+          <ul>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">Order</a></li>       
+          </ul>               
+        </div>
+        <div className="burger" onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+        </div>
+      </nav>
+      <style jsx>{`
+        .navbar {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          background: #16f4d7;
+          height: 60px
+        }
+        .nav_logo a {
+          color: white;
+          text-decoration: none;
+          font-size: 1.5rem;
+        }
+        .nav_navigation {
+          display: flex;
+          height: 100%;
+          align-items: center;
+          padding: 0 1rem;
+        }
+        .nav_items ul {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+          display: flex;
+        } 
+        .nav_items li {
+          padding: 0 0.5rem;
+        }
+        .nav_items a {
+          color: white;
+          text-decoration: none;
+        }
 
-    <nav className="nav_navigation">
-      <div></div>
-      <div className="nav_logo"><a href="/">Daiper-Cakez</a></div>
-      <div className="space"></div>
-      <div className="nav_items">
-        <ul>
-          <li><a href="/">About</a></li>
-          <li><a href="/">Contact</a></li>
-          <li><a href="/">Order</a></li>
-          <li><div className="Test" onClick={handleClick}>
-          {click ? <FaTimes /> : <FaBars />}
-        </div></li>
-        </ul>
-        
-      </div>
-    </nav>
-    <style jsx>{`q
-      .navbar {
-        box-shadow: 0px 2px 5px 10px rgba(0, 0, 0, 0.2)
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        background: #16f4d7;
-        height: 60px
-      }
-      .nav_logo a {
-        color: white;
-        text-decoration: none;
-        font-size: 1.5rem;
-      }
-      .nav_navigation {
-        display: flex;
-        height: 100%;
-        align-items: center;
-        padding: 0 1rem;
-      }
-      .nav_items ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-      } 
-      .nav_items li {
-        padding: 0 0.5rem;
-      }
-      .nav_items a {
-        color: white;
-        text-decoration: none;
-      }
+        .nav_items a:hover,
+        .nav_items a:active {
+          color: #fcb8f4;
+        }
+        .space {
+          flex: 1;
+        }
+        .burger {
+          color: pink;
+        }
 
-      .nav_items a:hover,
-      .nav_items a:active {
-        color: #fcb8f4;
-      }
-      .space {
-        flex: 1;
-      }
-    
-    `}</style>
+        @media (max-width: 768px) {
+          .nav_items {
+            display: none;
+          }
+        }
 
-  </header>
+        @media (min-width: 768px) {
+          .burger {
+            display: none;
+          }
+        }
+      
+      `}</style>
+
+    </header>
   )
 }
 
